08142fce806be57bed8990c9a6b0429005e24063
Jamile Cruz <130008726+milecruz26@users.noreply.github.com>
2024-01-12 11:14:07 -0300

    ajuste nas skills e no título

--- src/componets/SkillContainer/datas.js
+++ src/componets/SkillContainer/datas.js
@@ -4,37 +4,50 @@ import html from "../../assets/image/html.svg";
 import react from "../../assets/image/react.svg";
 import materialUi from "../../assets/image/material-ui.svg";
 import postgresql from "../../assets/image/postgresql.svg";
+import vite from "../../assets/image/vitejs.svg";
 
 const datas = [
   {
     id: 1,
+    name: "JavaScript",
     src: javascript,
     alt: "icon javascript",
   },
   {
     id: 2,
+    name: "CSS3",
     src: css,
     alt: "icon css",
   },
   {
     id: 3,
+    name: "HTML5",
     src: html,
     alt: "icon html",
   },
   {
     id: 4,
+    name: "React",
     src: react,
     alt: "icon reac",
   },
   {
     id: 5,
+    name: "Material UI",
     src: materialUi,
-    alt: "icon reac",
+    alt: "icon material ui",
   },
   {
     id: 6,
+    name: "PostgreSQL",
     src: postgresql,
-    alt: "icon reac",
+    alt: "icon postgresql",
+  },
+  {
+    id: 7,
+    name: "ViteJs",
+    src: vite,
+    alt: "icon vite",
   },
 ];
 
